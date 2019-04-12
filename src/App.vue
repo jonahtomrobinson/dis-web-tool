69ae2013e857a4a7534439ccceda84474bfc1c6f
Jonah Robinson <jonahtomrobinson@gmail.com>
2019-04-12 16:33:07 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<!--<style>
+  @import './assets/nav-bar.scss';
+</style>-->
+
+<template>
+  <div id="app">
+    <b-navbar toggleable="md" type="dark" variant="dark">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand to="/">My Vue App</b-navbar-brand>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item to="/posts-manager">Posts Manager</b-nav-item>
+          <b-nav-item to="/technologies">Technologies</b-nav-item>
+          <b-nav-item to="/ctf-events">CTF Events</b-nav-item>
+          <b-nav-item to="/infrastructure-creator">Infrastructure Creator</b-nav-item>
+          <b-nav-item href="#" @click.prevent="login" v-if="!activeUser">Login</b-nav-item>
+          <b-nav-item href="#" @click.prevent="logout" v-else>Logout</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <!-- routes will be rendered here -->
+    <router-view />
+    <p>hello</p>
+  </div>
+</template>
+
+<style>
+    p{
+        background:black;   
+    }
+</style>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      activeUser: null
+    }
+  },
+  async created () {
+    await this.refreshActiveUser()
+  },
+  watch: {
+    // everytime a route is changed refresh the activeUser
+    '$route': 'refreshActiveUser'
+  },
+  methods: {
+    login () {
+      this.$auth.loginRedirect()
+    },
+    async refreshActiveUser () {
+      this.activeUser = await this.$auth.getUser()
+    },
+    async logout () {
+      await this.$auth.logout()
+      await this.refreshActiveUser()
+      this.$router.push('/')
+    }
+  }
+}
+</script>
